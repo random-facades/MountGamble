92c84bd5b897e1c2585305b5c1cc5187b929eb78
random-facades <random.facades@gmail.com>
2025-10-13 19:01:45 -0400

    hide original suits and add only basic colors

--- core/mtg_card_overrides.lua
+++ core/mtg_card_overrides.lua
@@ -1,34 +1,61 @@
 --Load config
 --local lovely = require("lovely")
 
+-- Hide original Suits
+
+local function disabled(self, args)
+   return false
+end
+
+SMODS.Suits["Diamonds"].in_pool = disabled
+SMODS.Suits["Hearts"].in_pool = disabled
+SMODS.Suits["Spades"].in_pool = disabled
+SMODS.Suits["Clubs"].in_pool = disabled
+
+
+-- create new suits
 
 local mtg_suits = {
-   {key = 'Red', color = 'd11d11', basic = true},
-   {key = 'Blue', color = 'd11d11', basic = true},
-   {key = 'White', color = 'd11d11', basic = true},
-   {key = 'Black', color = 'd11d11', basic = true},
-   {key = 'Green', color = 'd11d11', basic = true},
-   {key = 'Colorless', color = 'd11d11', basic = false},
+   { key = 'Red',       color = 'f83b2f', basic = true },
+   { key = 'Blue',      color = '008ee6', basic = true },
+   { key = 'White',     color = 'e29000', basic = true },
+   { key = 'Black',     color = '3f1c46', basic = true },
+   { key = 'Green',     color = '429956', basic = true },
+   { key = 'Colorless', color = '4F6367', basic = false },
 }
+
+local function advanced_pool(self, args)
+   if args and (args.initial_deck or args.rank == '') then
+      return false
+   end
+   return true
+end
+
+local function basic_pool(self, args)
+   if args and args.rank == '' then
+      return false
+   end
+   return true
+end
 
 for i, v in ipairs(mtg_suits) do
    SMODS.Suit({
       key = v.key,
       card_key = v.key,
-      pos = { y = i-1 },
-      ui_pos = { x = i-1, y = 0 },
-      keep_base_colours = false,
-      lc_color = v.color,
-      hc_color = v.color,
+      pos = { y = i - 1 },
+      ui_pos = { x = i - 1, y = 0 },
+      keep_base_colours = true,
+      hidden = true,
       lc_atlas = 'base_suits',
       hc_atlas = 'base_suits',
       lc_ui_atlas = 'ui_assets',
       hc_ui_atlas = 'ui_assets',
-      in_pool = function(self, args)
-         if args and args.initial_deck then
-            return v.basic
-         end
-         return true
-      end,
+      in_pool = v.basic and basic_pool or advanced_pool,
    }):register()
+
+   local full_key = "mtg_" .. v.key
+   local suit_color = HEX(v.color)
+   G.C.SO_1[full_key] = suit_color
+   G.C.SO_2[full_key] = suit_color
+   G.C.SUITS[full_key] = suit_color
 end
---
+++ localization/en-us.lua
@@ -0,0 +1,29 @@
+-- Welcome to en-us.lua!
+-- friendly reminder that in the us we say things like "color" and not "colour"
+-- This is also the default file, if there are things here that are "missing" from other files it will use this one instead
+
+
+return {
+   misc = {
+      dictionary = {
+         k_maybe_next_time = "Maybe Next Time?",
+      },
+      -- Unown Card Suits
+      suits_plural = {
+         mtg_White = "Plains",
+         mtg_Blue = "Islands",
+         mtg_Red = "Mountains",
+         mtg_Black = "Swamps",
+         mtg_Green = "Forests",
+         mtg_Colorless = "Nothing",
+      },
+      suits_singular = {
+         mtg_White = "Plain",
+         mtg_Blue = "Island",
+         mtg_Red = "Mountain",
+         mtg_Black = "Swamp",
+         mtg_Green = "Forest",
+         mtg_Colorless = "Nothing",
+      },
+   }
+}
