13fcfdb4de47430409e79cf5d3b26c83e6560d1d
random-facades <random.facades@gmail.com>
2025-10-13 13:51:16 -0400

    add custom icon

---
+++ core/mtg_sprites.lua
@@ -0,0 +1,10 @@
+--Load config
+--local lovely = require("lovely")
+
+--Load all Atlas
+SMODS.Atlas({
+    key = "modicon",
+    path = "icon.png",
+    px = 33,
+    py = 33
+})
--- mountgamble.lua
+++ mountgamble.lua
@@ -9,3 +9,11 @@ mtgamble = {}
 mod_dir = ''..SMODS.current_mod.path
 mtgamble_config = SMODS.current_mod.config
 
+-- Load Core
+
+local sprites, load_error = SMODS.load_file("core/mtg_sprites.lua")
+if load_error then
+  sendDebugMessage ("The error is: "..load_error)
+else
+  sprites()
+end
